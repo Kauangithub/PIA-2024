43ff38621f349e1557df9176339e5eeac032fa54
Kauan <machadob.kauan@gmail.com>
2024-04-04 15:00:50 -0300

    teste js admin

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,11 @@
-document.get
+btnAdd = document.getElementsByClassName('add')
+forms = document.getElementById('span')
 
-document.getElementById('ticketForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio padrão do formulário
+function addingresso(){
+    if (forms.style.display == 'none'){
+        forms.style.display = 'block';
+    } else {
+        forms.style.display = 'none'
+    }
+}
 
-    // Captura os valores do formulário
-    var ticketName = document.getElementById('ticketName').value;
-    var ticketDescription = document.getElementById('ticketDescription').value;
-    var ticketPrice = document.getElementById('ticketPrice').value;
-    var ticketImage = document.getElementById('ticketImage').files[0];
-
-    // Aqui você pode enviar os dados para o servidor usando AJAX, por exemplo
-    // Por enquanto, vamos apenas exibir os dados no console
-    console.log('Nome:', ticketName);
-    console.log('Descrição:', ticketDescription);
-    console.log('Preço:', ticketPrice);
-    console.log('Imagem:', ticketImage);
-
-    // Limpa o formulário
-    document.getElementById('ticketForm').reset();
-});
