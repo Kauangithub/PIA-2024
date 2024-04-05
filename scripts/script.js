77c2da4a6f1b2b281ba6e2cd25b2775ac5e61a7c
Kauan <machadob.kauan@gmail.com>
2024-04-05 13:50:07 -0300

    Interatividade admin adicionar ingresso

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,29 @@
-btnAdd = document.getElementsByClassName('add')
+btnAdd = document.getElementById('add')
+btnRem = document.getElementById('rem')
 forms = document.getElementById('span')
+existe = document.getElementById('ingexistentes')
+lista = document.getElementById('listaIng')
+let nome = getElementById('ticketName')
 
-function addingresso(){
-    if (forms.style.display == 'none'){
-        forms.style.display = 'block';
+function addIng(){
+    if (forms.style.display = 'none'){
+        forms.style.display = 'block'
+        btnRem.style.display = 'none'
+        btnAdd.style.width = '45%'
+        btnAdd.style.margin =  '10px'
     } else {
-        forms.style.display = 'none'
-    }
-}
+        alert('Ingresso Criado')
+        lista.document.createElement('li')
+        lista.append('nome')
 
+    }  
+}
+function remIng(){
+    existe.style.display = 'block'
+}
+function canc(){
+    forms.style.display = 'none'
+}
+function test(){
+
+}
