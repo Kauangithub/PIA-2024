69858bc495ae547825ddf7a630b7c9abf9a6feac
Kauan <machadob.kauan@gmail.com>
2024-04-12 13:23:47 -0300

    bug css resolvido

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
-btnAdd = document.getElementById('add')
-btnRem = document.getElementById('rem')
-forms = document.getElementById('span')
-existe = document.getElementById('ingexistentes')
-lista = document.getElementById('listaIng')
-let nome = getElementById('ticketName')
+const btnAdd = document.getElementById('add')
+const btnRem = document.getElementById('rem')
+const forms = document.getElementById('span')
+const existe = document.getElementById('ingexistentes')
+const lista = document.getElementById('listaIng')
+const nome = document.getElementById('ticketName')
 
 function addIng(){
     if (forms.style.display = 'none'){
@@ -15,15 +15,14 @@ function addIng(){
         alert('Ingresso Criado')
         lista.document.createElement('li')
         lista.append('nome')
-
     }  
 }
 function remIng(){
     existe.style.display = 'block'
 }
-function canc(){
-    forms.style.display = 'none'
-}
-function test(){
-
+function cancel(){
+    forms.style.display = 'none';
+    btnRem.style.display = 'block'
+    btnAdd.style.width = '100%'
+    btnAdd.style.margin =  '0px'
 }
