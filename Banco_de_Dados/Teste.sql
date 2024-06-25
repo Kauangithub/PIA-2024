1dbf41b42a0907d7a4a3766106d60e1e507bde19
Rafael Ribeiro <klkp.398@gmail.com>
2024-06-24 21:32:48 -0300

    atualizado Teste.sql

--- Banco_de_Dados/Teste.sql
+++ Banco_de_Dados/Teste.sql
@@ -49,10 +49,10 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
-INSERT INTO Cadastro_ingresso (nome, descricao, preco, img) VALUES ('Teste01', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', '10', 'Teste01.jpg');
-INSERT INTO Cadastro_ingresso (nome, descricao, preco, img) VALUES ('Teste02', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', '20', 'Teste02.jpg');
-INSERT INTO Cadastro_ingresso (nome, descricao, preco, img) VALUES ('Teste03', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', '100', 'Teste03.jpg');
-INSERT INTO Cadastro_ingresso (nome, descricao, preco, img) VALUES ('Teste04', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', '50', 'Teste04.jpg');
+INSERT INTO Cadastro_ingresso (nome, descricao, preco, img) VALUES ('Teste01', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', 10, 'Teste01.jpg');
+INSERT INTO Cadastro_ingresso (nome, descricao, preco, img) VALUES ('Teste02', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', 20, 'Teste02.jpg');
+INSERT INTO Cadastro_ingresso (nome, descricao, preco, img) VALUES ('Teste03', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', 100, 'Teste03.jpg');
+INSERT INTO Cadastro_ingresso (nome, descricao, preco, img) VALUES ('Teste04', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', 50, 'Teste04.jpg');
 
 
 
